b2bab01f9ae9cb9fae6e84769518fa86d3dcbd04
niyasmohammed046 <niyasmohammed046@gmail.com>
2023-07-10 09:22:29 +0530

    finished

--- crmapp/urls.py
+++ crmapp/urls.py
@@ -6,6 +6,11 @@ urlpatterns = [
     path('',views.homepage,name='homepage'),
     # path('login/',views.login_user,name='login'),
     path('logout/',views.logout_user,name='logout'),
-    path('register',views.register_user,name='register'),
+    path('register/',views.register_user,name='register'),
+    path('record/<int:pk>/',views.customer_record,name='customer_record'),
+    path('delete_record/<int:pk>/',views.delete_customer_record,name='delete_customer_record'),
+    path('add_record/',views.add_record,name='add_record'),
+    path('update_record/<int:pk>/',views.update_record,name='update_record'),
+    
 
 ]
